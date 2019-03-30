c8eedf86dc43c55b98705b9056262999a574f45a
davidmutonga <nedelmutonga@gmail.com>
2019-03-30 17:37:28 +0300

    chat application

---
+++ app/src/main/java/com/example/whatsappclone/UserObject.java
@@ -0,0 +1,20 @@
+package com.example.whatsappclone;
+
+public class UserObject {
+
+    private String name;
+    private String phone;
+
+    public UserObject(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
